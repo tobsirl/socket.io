064871e842338b898d0e26e7b418efc26c1d0d43
Paul Tobin <tobster111@gmail.com>
2019-05-25 12:02:18 +0100

    Added a click event to the icons for different namespaces

--- socketIO201/slack/public/scripts.js
+++ socketIO201/slack/public/scripts.js
@@ -4,6 +4,22 @@ const socket = io('http://localhost:9000');
 socket.on('namespaceList', nsData => {
   console.log(`The list of namespaces has arrived!`);
   // console.log(nsData);
+  let namespacesDiv = document.querySelector('.namespaces');
+  namespacesDiv.innerHTML = '';
+  nsData.forEach(namespace => {
+    namespacesDiv.innerHTML += `<div class="namespace" ns=${
+      namespace.endpoint
+    }><img src="${namespace.img}"></div>`;
+  });
+
+  // Add a clicklistener for each namespace
+  Array.from(document.getElementsByClassName('namespace')).forEach(element => {
+    // console.log(element);
+    element.addEventListener('click', e => {
+      const nsEndpoint = element.getAttribute('ns');
+      console.log(`${nsEndpoint} I should go to now`);
+    });
+  });
 });
 
 socket.on('messageFromServer', dataFromServer => {
