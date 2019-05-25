be6307d443c048e59654239e46239a75634dffc1
Paul Tobin <tobster111@gmail.com>
2019-05-25 10:24:27 +0100

    Loop through the namespaces class

--- socketIO201/slack/slack.js
+++ socketIO201/slack/slack.js
@@ -4,12 +4,24 @@ const socketio = require('socket.io');
 
 let namespaces = require('./data/namespaces');
 
-console.log(namespaces);
+// console.log(namespaces);
 
+// use the static files in public
 app.use(express.static(__dirname + '/public'));
 
+
 const expressServer = app.listen(9000);
+
 const io = socketio(expressServer);
+
+// loop through each namespace and listen for a connection
+namespaces.forEach(namespace => {
+  // console.log(namespace.endpoint);
+  io.of(namespace.endpoint).on('connection', socket => {
+    console.log(`${socket.id} has joined ${namespace.endpoint}`);
+  });
+});
+
 io.on('connection', socket => {
   socket.emit('messageFromServer', { data: 'Welcome to the socketio server' });
   socket.on('messageToServer', dataFromClient => {
