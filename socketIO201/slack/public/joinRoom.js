52e0f485de9308f09b8f6a78b0182efaf8723199
Paul Tobin <tobster111@gmail.com>
2019-05-27 10:41:19 +0100

    Message history in the chatroom remands after a refresh

--- socketIO201/slack/public/joinRoom.js
+++ socketIO201/slack/public/joinRoom.js
@@ -6,4 +6,15 @@ function joinRoom(roomName) {
       '.curr-room-num-users'
     ).innerHTML = `${newNumberofMembers} <span class="glyphicon glyphicon-user"></span>`;
   });
+
+  nsSocket.on('historyCatchUp', history => {
+    // console.log(history);
+    const messagesUl = document.querySelector('#messages');
+    messagesUl.innerHTML = '';
+    history.forEach(msg => {
+      const newMsg = buildHTML(msg);
+      const currentMessages = messagesUl.innerHTML;
+      messagesUl.innerHTML = currentMessages + newMsg;
+    });
+  });
 }
