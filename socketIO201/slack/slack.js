8bc7ed82ee250641e458f3d7e319ac38a793c492
Paul Tobin <tobster111@gmail.com>
2019-05-27 10:25:02 +0100

    Using history to store chat messages

--- socketIO201/slack/slack.js
+++ socketIO201/slack/slack.js
@@ -61,6 +61,12 @@ namespaces.forEach(namespace => {
       // this is because the socket ALWAYS joins its own room on connection
       // get the keys
       const roomTitle = Object.keys(nsSocket.rooms)[1];
+      // we need to find the Room object for this room
+      const nsRoom = namespaces[0].rooms.find(room => {
+        return room.roomTitle === roomTitle;
+      });
+      console.log(nsRoom);
+      nsRoom.addMessage(fullMsg);
       io.of('/wiki')
         .to(roomTitle)
         .emit('messageToClients', fullMsg);
