4d42ec5c16c1e37d09ee8fb2f50ebc0665e10240
Paul Tobin <tobster111@gmail.com>
2019-05-28 11:36:57 +0100

    Installed and setup socket.io

--- AgarClone/server.js
+++ AgarClone/server.js
@@ -3,3 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+const socketio = require('socket.io');
+const expressServer = app.listen(8080);
+const io = socketio(expressServer);
