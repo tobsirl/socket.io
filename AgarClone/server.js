d168eeb1e1cc3aa9be46dfb01d71980712e58611
Paul Tobin <tobster111@gmail.com>
2019-05-28 12:26:48 +0100

    Added folders for socket.io and express files

--- AgarClone/server.js
+++
@@ -1,9 +0,0 @@
-// Agar.io clone
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-const socketio = require('socket.io');
-const expressServer = app.listen(8080);
-const io = socketio(expressServer);
