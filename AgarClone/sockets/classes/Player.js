0ebdd430b426c4cae1dddb20fb59335059fafe4b
Paul Tobin <tobster111@gmail.com>
2019-05-29 12:07:45 +0100

    Finished the playerConfig class

--- AgarClone/sockets/classes/Player.js
+++ AgarClone/sockets/classes/Player.js
@@ -1,1 +1,6 @@
 // this is where ALL the data is stored about a given player
+class Player{
+  constructor() {
+    
+  }
+}
--- AgarClone/sockets/classes/PlayerConfig.js
+++ AgarClone/sockets/classes/PlayerConfig.js
@@ -1,1 +1,11 @@
 // this is where all the data is that no other player needs to know about
+class PlayerConfig {
+  constructor(settings) {
+    this.xVector = 0;
+    this.yVector = 0;
+    this.speed = settings.defaultSpeed;
+    this.zoom = settings.defaultZoom;
+  }
+}
+
+module.exports = PlayerConfig;
