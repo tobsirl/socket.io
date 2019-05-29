56003bcf96b368a0468f573fc2d71cc4aaa3a4c9
Paul Tobin <tobster111@gmail.com>
2019-05-29 10:28:02 +0100

    Added the initGame() function to socketMain.js

--- AgarClone/sockets/socketMain.js
+++ AgarClone/sockets/socketMain.js
@@ -4,4 +4,11 @@ const io = require('../servers').io;
 // import the Orb class
 const Orb = require('./classes/Orb');
 
+// run at the beginning of a new game
+function initGame() {
+  for (let i = 0; i < 500; i++) {
+    orbs.push(new Orb());
+  }
+}
+
 module.exports = io;
