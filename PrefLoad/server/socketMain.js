86409d232d31e7280ff9219ef4ad6c95e3fd4406
Paul Tobin <tobster111@gmail.com>
2019-06-02 13:43:25 +0100

    Added checkAndAdd to save the data to mongo

--- PrefLoad/server/socketMain.js
+++ PrefLoad/server/socketMain.js
@@ -23,11 +23,35 @@ function socketMain(io, socket) {
   // a machine has connected, check to see if it's new.
   // if it is, add it
   socket.on('initPerfData', data => {
-    data.macA
+    // update our socket connect function scoped variable
+    macA = data.macA;
+    // now go check mongo!
+    checkAndAdd(data);
   });
 
   socket.on('perfData', data => {
     console.log(data);
+  });
+}
+
+function checkAndAdd(data) {
+  // because we are doing db stuff, js won't wait for the db
+  // so we need to make this a promise
+  return new Promise((resolve, reject) => {
+    Machine.findOne({ macA: data.macA }, (err, doc) => {
+      if (err) {
+        throw err;
+        reject(err);
+      } else if (doc === null) {
+        // the record is not in the db, so add it!
+        let newMachine = new Machine(data);
+        newMachine.save(); // save it to the database
+        resolve('added');
+      } else {
+        // it is in the db just resolve
+        resolve('found');
+      }
+    });
   });
 }
 
