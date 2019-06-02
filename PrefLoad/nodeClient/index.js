5416a015b12f50856cee2cdf2cbb796abe43e365
Paul Tobin <tobster111@gmail.com>
2019-06-02 11:02:52 +0100

    Using os.networkInterfaces() to get access to unique mac address

--- PrefLoad/nodeClient/index.js
+++ PrefLoad/nodeClient/index.js
@@ -9,7 +9,16 @@ const io = require('socket.io-client');
 const socket = io('http://127.0.0.1:8181');
 
 socket.on('connect', () => {
-  console.log(`I connected to the socket server... hooray!`);
+  // console.log(`I connected to the socket server... hooray!`);
+  // we need a way to identify this machine to whomever concerned
+  const nI = os.networkInterfaces()
+  let macA;
+  // loop through all the nI for this machine and fin a non-internal one
+  for (let key in nI) {
+    if (!nI[key][0].internal) {
+      
+    }
+  }
 });
 
 function performanceData() {
