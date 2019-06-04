1335113001613d1e9e500de1a0bfe3c9850c4ac4
Paul Tobin <tobster111@gmail.com>
2019-06-04 10:35:34 +0100

    Dispaying drawCircle in the Cpu Component

--- PrefLoad/client/src/components/Cpu.js
+++ PrefLoad/client/src/components/Cpu.js
@@ -3,10 +3,15 @@ import React from 'react';
 import drawCircle from '../utilities/canvasLoadAnimation';
 
 const Cpu = props => {
+  const canvas = document.querySelector('canvas');
+  drawCircle(canvas, props.cpuData.cpuLoad);
   return (
-    <div>
-      <h1>Cpu!</h1>
-      <p>{props.cpuData.cpuLoad}</p>
+    <div className="col-sm3 cpu">
+      <h3>Cpu Load!</h3>
+      <div className="canvas-wrapper">
+        <canvas className="canvas" />
+        <div class="cpu-text">{props.cpuData.cpuLoad}</div>
+      </div>
     </div>
   );
 };
